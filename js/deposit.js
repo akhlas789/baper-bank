75c7b8052fac2b243a397b8cf96ab9c917ec9c71
Akhlas789 <akhlasuddin54321@gmail.com>
2023-09-29 10:56:31 +0600

    deposit add

--- js/deposit.js
+++ js/deposit.js
@@ -1,3 +1,13 @@
 document.getElementById('btn-deposit').addEventListener('click',function(){
-    
+    const depositFild = document.getElementById('deposit-fild')
+    const newDepositAmountString = depositFild.value
+    const newDepositAmount = parseFloat(newDepositAmountString)
+
+    const depositTotalElement = document.getElementById('deposit-total')
+    const PreviusDepositTotalString = depositTotalElement.innerText
+    const PreviusDepositTotal = parseFloat(PreviusDepositTotalString)
+    const currentDepositTotal = newDepositAmount + PreviusDepositTotal
+    depositTotalElement.innerText = currentDepositTotal
+
+    depositFild.value = ''
 })
